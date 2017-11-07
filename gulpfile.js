6c9f87c15c23b19d0d3046e13e2c1b97e0c12ae2
pnochisaki <pnochisaki@gmail.com>
2017-11-07 13:59:58 -0800

    outcomes

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ handlebars.registerHelper('imgL', function(str) {
 });
 
 handlebars.registerHelper('pdfurl', function(str) {
-  return '//res.cloudinary.com/fiveten/image/upload/c_scale,q_auto:good,w_1500/'+str+'.jpg';
+  return '//res.cloudinary.com/fiveten/image/upload/c_scale,q_auto:good,w_1500/'+str+'.pdf';
 });
 
 handlebars.registerHelper('imgS', function(str) {
